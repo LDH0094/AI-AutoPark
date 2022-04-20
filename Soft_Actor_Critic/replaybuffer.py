91b5d9604829763a611cc83e57ab9ec98244d2ce
Deokhyun Lee <deok.lee@obf.ateneo.edu>
2022-04-20 21:41:25 +0900

    replaybuffer

--- Soft_Actor_Critic/replaybuffer.py
+++ Soft_Actor_Critic/replaybuffer.py
@@ -17,26 +17,25 @@ class ReplayBuffers(object):
         self.buffer = deque()
         self.count = 0
 
-    ## 버퍼에 저장
+    
     def add_data(self, state, action, reward, next_state, done):
         transition = (state, action.flatten(), reward, next_state, done)
         # print(list(transition))
 
-        # 버퍼가 꽉 찼는지 확인
+        # checks if buffer is full
         if self.count < self.buffer_size:
             self.buffer.append(transition)
             self.count += 1
-        else: # 찼으면 가장 오래된 데이터 삭제하고 저장
+        else: 
             self.buffer.popleft()
             self.buffer.append(transition)
         
-    ## 버퍼에서 데이터 무작위로 추출 (배치 샘플링)
+    
     def sample_batch(self, batch_size):
         if self.count < batch_size:
             batch = random.sample(self.buffer, self.count)
         else:
             batch = random.sample(self.buffer, batch_size)
-        # 상태, 행동, 보상, 다음 상태별로 정리
         states = np.asarray([i[0] for i in batch])
         actions = np.asarray([i[1] for i in batch])
         rewards = np.asarray([i[2] for i in batch])
@@ -45,13 +44,9 @@ class ReplayBuffers(object):
         return states, actions, rewards, next_states, dones
 
         
-
-    ## 버퍼 사이즈 계산
     def buffer_count(self):
         return self.count
 
-
-    ## 버퍼 비움
     def clear_buffer(self):
         self.buffer = deque()
         self.count = 0
